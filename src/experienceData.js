afa88fcc316060fe6ab35025a727011fe1178281
Rajeev Krishna <radzhivkrishna@gmail.com>
2024-11-09 21:02:31 +0530

    chore. Experience data

---
+++ src/experienceData.js
@@ -0,0 +1,48 @@
+export const experienceData = [
+    {
+        title: "Frontend Engineer Intern",
+        company: "NexusDevOps",
+        dates: "July 2024 - Present",
+        description: "Working as a Frontend Engineer, focusing on user interfaces and IDEs.",
+        responsibilities: [
+          "Developed the new user interface for the primary IDE.",
+          "Collaborated with the backend team to integrate new APIs.",
+          "Optimized components for better performance and accessibility.",
+          "Conducted user testing to gather feedback on new features."
+        ],
+        techStack: ["JavaScript", "Tailwind", "React", "Git", "Node", "AWS", "Figma"]
+      },
+      {
+        title: "Frontend Developer Intern",
+        company: "Judix",
+        dates: "July 2024 - July 2024",
+        description: "Focused on UI development and component building.",
+        responsibilities: [
+          "Developed responsive UI components using React and Tailwind.",
+          "Collaborated with designers to ensure brand consistency.",
+          "Optimized pages for faster load times on mobile devices."
+        ],
+        techStack: ["JavaScript", "Tailwind", "React", "NextJS"]
+      },
+    {
+      title: "Web Developer Intern",
+      company: "JoinDevOps",
+      dates: "March 2024 - May 2024",
+      description: "Responsible for developing the new user interface and IDEs for the respective discipline",
+      techStack: ["HTML", "CSS", "JavaScript", "Webflow"]
+    },
+    {
+      title: "UI/UX Designer",
+      company: "Freelance",
+      dates: "September 2023",
+      description: "Responsible for developing the new user interface and IDEs for the respective discipline",
+      techStack: ["Figma", "Prototyping"]
+    },
+    {
+      title: "Intern",
+      company: "Impetus",
+      dates: "August 2023 - October 2023",
+      description: "Responsible for developing the new user interface and IDEs for the respective discipline",
+      techStack: ["HTML", "CSS", "JavaScript", "Tailwind", "React", "Git"]
+    }
+  ];
