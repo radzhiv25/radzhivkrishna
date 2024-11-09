462a46fa0f60f787632e9b4ba7b77d265e016618
Rajeev Krishna <radzhivkrishna@gmail.com>
2024-11-09 21:02:55 +0530

    chore. Project data

---
+++ src/projectData.js
@@ -0,0 +1,48 @@
+// projectData.js
+export const projectData = [
+    {
+      name: "Crypto News",
+      link: "https://dribbble.com/shots/22615728-Crypto-News-Landing-Page",
+      source: "",
+      description: "A basic crypto landing page",
+      skills: ["Figma", "Prototyping"],
+      image: "../src/assets/Dribble(Mobile).png",
+      className: "md:col-span-2" // Example of a smaller card
+    },
+    {
+      name: "Pixus Payment",
+      link: "https://dribbble.com/shots/22281003-Landing-Page-UI",
+      source: "",
+      description: "Landing page for Card Payment Website",
+      skills: ["Figma"],
+      image: "../src/assets/LandingPage.png",
+      className: "md:col-span-3" // Example of a larger card
+    },
+    {
+      name: "Task Manager",
+      link: "https://alpha-task-manager.vercel.app",
+      source: "",
+      description: "A simple task manager to manage your daily tasks",
+      skills: ["React", "Tailwind"],
+      image: "../src/assets/TaskManager.png",
+      className: "" // Example of a smaller card
+    },
+    {
+      name: "NewsX",
+      link: "https://newsx-radzhiv.web.app",
+      source: "",
+      description: "Your daily news app",
+      skills: ["React", "Tailwind", "GNews API"],
+      image: "../src/assets/NewsX.png",
+      className: "md:col-span-3" // Example of a larger card
+    },
+    {
+      name: "PennWise",
+      link: "https://pennwise.vercel.app",
+      source: "",
+      description: "An expense tracker to manage your daily expenses",
+      skills: ["React", "Tailwind", "ChartJS"],
+      image: "../src/assets/PennWise.png",
+      className: "md:col-span-3" // Example of a smaller card
+    }
+  ];
