64a44860524a98c0b7cd0a7e785f342b20c280a5
Rajeev Krishna <radzhivkrishna@gmail.com>
2024-11-09 22:14:03 +0530

    chore. minor changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,19 @@
 import Footer from './components/Footer';
 import Hero from './components/Hero';
-import Skills from './components/Skills';
 import Navbar from './components/Navbar';
 import About from './pages/About';
 import Work from './pages/Work';
-import {Routes, Route} from 'react-router-dom';
+import { Switch, Route } from 'react-router-dom';
 
 function App(){
   return(
     <div className="p-5 md:w-3/4 mx-auto flex flex-col h-screen">
     <Navbar />
-    <Routes>
+    <Switch>
         <Route path="/" element={<Hero/>} />
         <Route path="/about" element={<About/>} />
         <Route path="/work" element={<Work/>} />
-    </Routes>
+    </Switch>
     <Footer />
     </div>
   )
